acf920577d40dd5c2d20fc6284e5e39ffe729a42
Han Xu <hanxu27@gmail.com>
2020-03-17 22:46:47 -0400

    net and stuff

--- src/components/zoneCard.js
+++ src/components/zoneCard.js
@@ -1,15 +1,32 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import {
+  FaArrowAltCircleDown,
+  FaArrowAltCircleUp,
+  FaArrowAltCircleLeft,
+  FaArrowAltCircleRight
+} from "react-icons/fa";
 
 const ZoneCard = props => {
+  const arrow = {
+    1: <FaArrowAltCircleLeft />,
+    2: <FaArrowAltCircleDown />,
+    3: <FaArrowAltCircleRight />,
+    4: <FaArrowAltCircleRight />,
+    5: <FaArrowAltCircleUp />,
+    6: <FaArrowAltCircleLeft />
+  };
   return (
     <Card
       style={{
-        width: "18rem",
-        maxHeight: "20rem"
+        maxHeight: "20rem",
+        width: "15rem",
+        opacity: "0.9"
       }}
     >
-      <Card.Title style={{ margin: "1rem" }}>{`Zone #${props.zone}`}</Card.Title>
+      <Card.Title style={{ margin: "1rem" }}>
+        {arrow[props.zone]} {`Zone ${props.zone}`}
+      </Card.Title>
       <Card.Body>
         <Button onClick={e => props.handleSubModal(e)} id={props.zone}>
           {props.lineup &&
